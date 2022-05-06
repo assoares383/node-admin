6032d69b966747f833deff0ef804f2f3e7ddd3c4
= <alexandressoares383@gmail.com>
2022-05-06 17:50:47 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"]
+}));
+
+routes(app);
+
+app.listen(9000, () => {
+  console.log('listening to port 9000');
+})
